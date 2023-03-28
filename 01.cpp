68e7f81024366fd2bc88e38c1769d26b44d631cc
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2023-03-29 01:14:20 +0530

    Add files via upload

--- 01.cpp
+++ 01.cpp
@@ -1,8 +1,28 @@
-#include<stdio.h>
+// Define a function to input variable length string and store it in an array without memory wastage.
+#include<iostream>
+// #include<stdio.h>
+using namespace std;
 
 int main()
 {
-    int a =90;
-    printf("hello");
+    cout<<"Enter your name : ";
+    int i=0,j=0;
+    char c, *str = (char*)malloc(sizeof(char));
+    while(c != '\n')
+    {
+        c = getc(stdin);
+        j++;
+        str = (char*)realloc(str, j * sizeof(char));
+        str[i] = c;
+        i++;
+    }
+    str[i] = '\0';
+    cout<<"Your name is : ";
+    i=0;
+    while(str != '\0')
+    {
+        cout<<str[i];
+        i++;
+    }
     return 0;
 }
